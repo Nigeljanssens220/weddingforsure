5e90726fae33fb5217bfa9ecb77989157a0050c9
Nigel Janssens <janssensnigel@gmail.com>
2023-12-31 13:44:18 +0100

    Add @vercel/speed-insights package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { GeistSans } from 'geist/font/sans'
 import { headers } from 'next/headers'
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
