6ae021107b283db228a8869569103e28cb3eb91a
Nigel Janssens <janssensnigel@gmail.com>
2024-02-04 17:07:49 +0100

    Add dynamic variable and update CSS class in RSVP page

--- src/app/(main)/rsvp/page.tsx
+++ src/app/(main)/rsvp/page.tsx
@@ -1,9 +1,11 @@
 import RsvpForm from '@/app/_components/rsvp-form'
 import Typography from '@/app/_components/ui/typography'
 
+export const dynamic = 'force-static'
+
 const RSVP = () => {
   return (
-    <div className="flex w-full flex-col items-center px-3 pb-4 pt-16 selection:bg-black selection:text-[#C78686]">
+    <div className="flex min-h-dvh w-full flex-col items-center px-3 pb-4 pt-16 selection:bg-black selection:text-[#C78686]">
       <div className="flex flex-col gap-4">
         <Typography className="max-w-lg text-balance text-4xl md:text-7xl" as="h1">
           Come dance with us, baby!
