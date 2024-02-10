1e7c74a20c3b3333dca6e66ddd542ac386baad42
Nigel Janssens <janssensnigel@gmail.com>
2024-02-10 17:03:35 +0100

    Refactor sleep section UI

--- src/sections/sleep-section.tsx
+++ src/sections/sleep-section.tsx
@@ -1,5 +1,4 @@
 import Typography from '@/app/_components/ui/typography'
-import Image from 'next/image'
 
 export function SleepSection() {
   return (
@@ -7,29 +6,16 @@ export function SleepSection() {
       id="sleep"
       className="min-h-h-dvh flex w-screen max-w-screen-md flex-col items-center gap-10 px-10 py-10 text-center "
     >
-      <Typography as="h3" className="text-3xl font-semibold">
-        Overnachten
-      </Typography>
-      <Typography as="p">
-        Willen jullie in de liefdesbubbel blijven met ons en dus nog niet terug naar het echte leven? Check de hotels in
-        de buurt.
-      </Typography>
-      <div className="grid grid-cols-2 gap-10">
-        <div>
-          <Typography as="h4">HOTEL BERGSE BOSSEN</Typography>
-          <Typography as="p">
-            Het hotel waar wij de nacht doorbrengen! Kom de volgende dag gezellig met ons nagenieten bij het ontbijt. En
-            wellicht daarna een boswandeling maken.
-          </Typography>
-        </div>
-        <div>
-          <Typography as="h4">LANDGOED DE HORST</Typography>
-          <Typography as="p">
-            Dit hotel is gunstig gelegen, met gratis parkeergelegenheid. Kamerprijzen vanaf €90,- inclusief ontbijt.
-          </Typography>
-        </div>
+      <div className="space-y-4">
+        <Typography as="h3" className="text-3xl font-semibold">
+          Overnachten
+        </Typography>
+        <Typography as="p">
+          Willen jullie in de liefdesbubbel blijven met ons en dus nog niet terug naar het echte leven? Check de hotels
+          in de buurt.
+        </Typography>
       </div>
-      <Image src="/images/map.png" alt="Map driebergen" width={262} height={205} />
+
       <Typography as="p">
         * Helaas is het voor ons niet mogelijk om voor iedereen overnachting te verzorgen. Eventuele overnachtingen
         dienen dus zelf geregeld en betaald te worden. Laat ons even weten als je een hotel hebt geboekt, dan kunnen wij
