bd798fc375926695519fdd957eb0979c9c45d910
Nigel Janssens <janssensnigel@gmail.com>
2024-02-19 10:30:50 +0100

    Update site title in siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: "Shinta & Nigel's Wedding",
+  title: 'The Nishi Wedding',
   description: "Shinta and Nigel's beautiful wedding day!",
   icons: [{ rel: 'icon', url: '/favicon.svg' }],
 }
