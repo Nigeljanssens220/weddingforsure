d33d789abc478ecfc6357b87fcd0f7a08c09919d
Nigel Janssens <janssensnigel@gmail.com>
2024-02-19 10:29:55 +0100

    Update favicon to use SVG format

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: "Shinta & Nigel's Wedding",
   description: "Shinta and Nigel's beautiful wedding day!",
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+  icons: [{ rel: 'icon', url: '/favicon.svg' }],
 }
